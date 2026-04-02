11c1194528cd61a86bdd5f3a01e27e19d163f4b1
samdone763 <samdone763@gmail.com>
2026-04-03 02:59:38 +0300

    Fix psycopg2 for Render

--- app/db/session.py
+++ app/db/session.py
@@ -1,6 +1,3 @@
-from psycopg2cffi import compat
-compat.register()
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
